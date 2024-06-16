c4a8d4615987e8d0d458803c4b03a5e4d66a4b37
TheLuis <luisdavidgilcabarcas47@.com>
2024-06-16 15:07:43 -0500

    middleware y .env

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const usuarioRoutes = require('./routes/usuarioRoutes');
-const inventarioRoutes = require('./routes/inventarioRoutes');
-const estadoEquipoRoutes = require('./routes/estadoEquipoRoutes');
-const marcaEquipoRoutes = require('./routes/marcaEquipoRoutes');
-const tipoEquipoRoutes = require('./routes/tipoEquipoRoutes');
 const bodyParser = require('body-parser');
+require('./database/database');
+
+const usuarioRoutes = require('./src/routes/usuarioRoutes');
+const inventarioRoutes = require('./src/routes/inventarioRoutes');
+const estadoEquipoRoutes = require('./src/routes/estadoEquipoRoutes');
+const marcaEquipoRoutes = require('./src/routes/marcaEquipoRoutes');
+const tipoEquipoRoutes = require('./src/routes/tipoEquipoRoutes');
 
 const index = express();
 
 // Middleware
 index.use(bodyParser.json());
-
-// Conectamos la base de datos
-mongoose.connect('mongodb://localhost:27017/tu_base_de_datos', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(() => console.log('MongoDB conectado'))
-  .catch(err => console.log(err));
 
 index.use('/api/usuarios', usuarioRoutes);
 index.use('/api/inventarios', inventarioRoutes);
